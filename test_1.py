eda11396131e93db969acd9f328b191f317b4f71
noob2 <kirillxshilov@gmail.com>
2020-08-11 23:02:56 +0300

    now works

--- test_1.py
+++ test_1.py
@@ -1,27 +1,30 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import os.path
 
 def test_1():
-    driver = webdriver.Chrome()
-    driver.get("http://duckduckgo.com")
-    elem = findElement(By.id("search_form_input_homepage"))
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+      
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get('http://duckduckgo.com')
+    elem = driver.find_element(By.ID, 'search_form_input_homepage')
     elem.clear()
-    elem.send_keys("docker")
+    elem.send_keys('docker')
     elem.send_keys(Keys.RETURN)
-    links = driver.findElements(By.tagName("a"))
+    data = driver.find_element_by_xpath('//*[@id="r1-0"]/div/div[1]/div/a')
     try:
-        assert "https://www.docker.com/" in links
-        result = "ok"
+        assert 'https://www.docker.com' == data.text
+        result = 'ok\n'
     except Exception as e:
         result = e
     finally:
-        mode = "a" if os.path.exists("result_test_1") else "w"
-        with open("result_test_1", mode) as f:
-            f.write(result)
+        mode = 'a' if os.path.exists('result_test_1') else 'w'
+        with open('result_test_1', mode) as f:
+            f.write(str(result))
         driver.close()
-
-
-    
-
